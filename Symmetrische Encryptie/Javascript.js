2ea5173c9cd0678e0e903ad8a2ca14dd14d27b13
Alec <vanoosterwijckalec@hotmail.com>
2022-04-28 10:53:52 +0200

    Bespreken code met damiaan

--- Symmetrische Encryptie/Javascript.js
+++ Symmetrische Encryptie/Javascript.js
@@ -1,31 +1,40 @@
-const iv = CryptoJS.enc.Hex.parse('fedcba0987654321fedcba0987654321');
 
 function encrypteer() {
-  var key = CryptoJS.enc.Hex.parse(getRanHex(32));
 
+  var encrypted = CryptoJS.AES.encrypt("**MySEcret stuff", "Secret Passphrase");
+    console.log("encrypted",encrypted);
+var decrypted = CryptoJS.AES.decrypt(encrypted, "Secret Passphrase");
+console.log("decrypted", CryptoJS.enc.Utf8.stringify( decrypted));
+
+  //var key = CryptoJS.enc.Hex.parse(generateKey(32));
+  var key = generateKey(32);
+  document.getElementById("key").innerHTML = key //gaat het encryptiesleutel tonen
+  
   var msg = document.getElementById("text").value;
 
-  var enc = CryptoJS.AES.encrypt(msg,key,{
-    iv,
-    mode: CryptoJS.mode.CBC,
-    padding: CryptoJS.pad.Pkcs7,
-  });
-  document.getElementById("EncryptedValue").innerHTML = enc //gaat het geëncypteerd bericht tonen
-  document.getElementById("key").innerHTML = key //gaat het encryptiesleutel tonen
+  var enc = CryptoJS.AES.encrypt(msg,key);
+
+  // TODO: CALL API to store ENC and retrieve ID
+  var id = 0;
+
+  // TODO: show url to user : "/index.html#id=" + id + "&key="+ key
+
+  // for debugging purposes 
+  document.getElementById("EncryptedValue").innerHTML =  enc //gaat het geëncypteerd bericht tonen
 }
 
 function decrypteer() {
   var encryptedMessage = document.getElementById("EncryptedValue").innerHTML;
   var key = document.getElementById("key").innerHTML; //krijgt een string terug
-  var hexkey = stringToHex(key)
+  // var hexkey = stringToHex(key)
 
-  var decrypt = CryptoJS.AES.decrypt(encryptedMessage,hexkey);
+  var decrypt = CryptoJS.AES.decrypt(encryptedMessage, key);
   document.getElementById("decrypted").innerHTML = decrypt;//toont het bericht
   console.log(decrypt)
   
 }
 
-const getRanHex = size => {
+const generateKey = size => {
   let result = [];
   let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
 
@@ -35,16 +44,17 @@ const getRanHex = size => {
   console.log(result)
   return result.join('');
 }
-function stringToHex(key){
-  let result = [];
 
-  for (let i = 0; i < key.length; i++) {
-    result.push(key[i] = " " + key[i]);
-  }
-  console.log(result)
+// function stringToHex(key){
+//   let result = [];
 
-  return result.join('');
-}
+//   for (let i = 0; i < key.length; i++) {
+//     result.push(key[i] = " " + key[i]);
+//   }
+//   console.log(result)
+
+//   return result.join('');
+// }
 
 //dit maakt een random 16 bit hexa getal aan. Die we nodig hebben voor een random Encryptie sleutel
 
